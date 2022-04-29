08a21cbbfdca83e846ee26496051367bf662adcd
German Carosio <GCAROSIO@claro.com.ar>
2022-04-29 16:51:00 -0300

    subiendo proyecto a repositorio nuevo con mejoras

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require("path")
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || port, () => {
+  console.log("Levantando un servidor con Express")
+});
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/productDetail", (req,res)=>{
+  res.sendFile(__dirname + "/views/productDetail.html")
+})
+
+app.get("/login", (req,res)=>{
+  res.sendFile(__dirname + "/views/login.html")
+})
+
+app.get("/register", (req,res)=>{
+  res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/productCart", (req,res)=>{
+  res.sendFile(__dirname + "/views/productCart.html")
+})
