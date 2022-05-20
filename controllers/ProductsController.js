465e36c4ef5f6cefd19f468ad9d3fae2775e8265
German Carosio <carosiogerman@gmail.com>
2022-05-19 22:39:52 -0300

    avances en crud de productos

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
+const path = require('path');
 
-const productos = JSON.parse(fs.readFileSync('./productos.json'))
+//leer json y crear un array en js
+const productosJsonRead = fs.readFileSync('./productos.json',{encoding:'utf-8'});
+let productos;
+
+if (productosJsonRead == "") {
+     productos = [];
+}else {
+     productos = JSON.parse(productosJsonRead);
+}
 
 //categorias
-
 const categorias = ["Headset", "Mouse", "Teclado"];
 
+//array de carrito
 let carrito = [];
 
 let ProductsController = {
    
+//listar productos 
     productList: (req,res) => {
      res.render("productList",{productos: productos, categorias: categorias});
      },
 
+//detalle de producto
     productDetail: (req,res) => {
          const producto = productos.find(element =>{
                return element.id === parseInt(req.params.id)
@@ -21,6 +32,7 @@ let ProductsController = {
          res.render("productDetail",{producto: producto});
      },
 
+//dividir en categorias
     categorias: (req,res) => {  
 
      const categoria = productos.filter(element =>{
@@ -29,16 +41,58 @@ let ProductsController = {
          res.render("categorias",{categoria:categoria,categorias: categorias});
     },
 
+//administrador
      administrador:(req,res) => {
      res.render("administrador", {productos: productos});
 },
 
-     crear:(req,res) => {
-     res.render("crear");
+//crear producto
+     productCreate:(req,res) => {+
+          
+     res.render("productCreate", {productos:productos});
 },
+     productSave:(req,res) => {
+
+     productoNuevo = {
+          id: req.body.id,
+          marca: req.body.marca,
+          modelo:req.body.modelo,
+          precio: req.body.precio,
+          detalle: req.body.detalle,
+          categoria:req.body.categoria,
+          img: req.body.img
+     };
+
+     productos.push(productoNuevo);
+
+     console.log(productos);
+
+     const productosJsonRead = JSON.stringify(productos);
+     fs.writeFileSync('./productos.json', productosJsonRead);
+
+     res.redirect("administrador");
+},
+
+//Editar producto 
+     productEdit:(req,res) => {
+     const producto = productos.find(element =>{
+     return element.id === parseInt(req.params.id)
+     })
+     res.render("productEdit",{productoEdit: producto});
+},
+
+//Eliminar producto 
+     productDelete:(req,res) => {
+          
+          res.send("eliminar");
+},
+
+//carrito de compras
     productCart: (req,res) => {
+
      res.render("productCart",{carrito: carrito});
-     }
+}
+
 }
 
 
--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,31 @@
 //llamar al router de express
 const express = require("express");
 const router = express.Router();
+//llamar a multer (procesar archivos)
+const multer = require("multer");
 
 //llamar al controlador
 const ProductsController = require ("../controllers/ProductsController")
 
-//crear las rutas para "/..."  (app.get)
+//crear las rutas 
 
+//productos
 router.get("/productList", ProductsController.productList);
 router.get("/productDetail/:id", ProductsController.productDetail);
 router.get("/categorias/:categoria", ProductsController.categorias);
+
+//carrito
 router.get("/productCart", ProductsController.productCart);
 
 //administrador
 router.get("/administrador", ProductsController.administrador);
-router.get("/crear", ProductsController.crear);
 
+router.get("/productCreate", ProductsController.productCreate);
+router.post("/productCreate", ProductsController.productSave);
+
+router.get("/productEdit/:id", ProductsController.productEdit);
+
+router.delete("/delete/:id", ProductsController.productDelete);
 
 
 
